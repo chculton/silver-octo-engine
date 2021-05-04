620fe09eb454f3ad0507747871f7e0be0021a83c
C. Hans Culton <chansculton@gmail.com>
2021-05-04 13:14:15 -0800

    Server connects to MongoDB cluster

--- server/index.js
+++ server/index.js
@@ -9,4 +9,12 @@ app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 
-// TO-DO: link to MongoDB cluster
+// TO-DO: convert to environmental variable
+const CONNECTION_URL = "mongodb+srv://MERNTestUser1:MERNTestUser-48@cluster0.sk3kn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log("Server running on port: ", PORT)))
+    .catch((error)=> console.log(error.message));
+
+mongoose.set("useFindAndModify", false);

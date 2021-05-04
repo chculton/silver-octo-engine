c4c38329b538ee821d42e8d6211d18f0b82ae6e9
C. Hans Culton <chansculton@gmail.com>
2021-05-03 16:25:00 -0800

    Declared Express app and commented out body-parser import

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser'; deprecated, automatically a part of express version 4.16 and higher
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+// TO-DO: link to MongoDB cluster
